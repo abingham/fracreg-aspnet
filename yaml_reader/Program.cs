b056c785e38e67cdf375c647cdc2e71adb4fc86b
Austin Bingham <austin.bingham@gmail.com>
2015-06-22 11:25:10 +0200

    Fixed typo in yaml cli loader.

--- yaml_reader/Program.cs
+++ yaml_reader/Program.cs
@@ -7,7 +7,7 @@ namespace yaml_reader
         public static void Main (string[] args)
         {
             var filename = args [0];
-            foreach (var aoInfo in AOInfolLoader.Load(filename)) {
+            foreach (var aoInfo in AOInfoLoader.Load(filename)) {
                 Console.WriteLine (string.Format ("{0}, {1}, {2}", 
                     aoInfo.code, 
                     aoInfo.fullName, 
